e01c4788375ebc3e8be56c004e7ea629bc6ef15e
Noshei <8866000+Noshei@users.noreply.github.com>
2021-05-22 15:03:19 -0600

    WMSS

--- Features/Wild-Magic-Surge-Spells.js
+++ Features/Wild-Magic-Surge-Spells.js
@@ -1,6 +1,7 @@
 // Wild Magic Surge Spells
 // Manually called macro
 // Requires Wild Magic Surge Spells actor with all of the spells, should be configured with same stats as the sorcerer
+// https://github.com/Noshei/Foundry-Stuff/blob/main/Features/Wild_Magic_Surge_Spells-Actor.json
 
 let actorD = game.actors.find(i => i.name === `Wild Magic Surge Spells`);
 let spells = actorD.items.filter(i => i.type === "spell");
